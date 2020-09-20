ec84078e6d774ffb62b136991e9c0a245d3a2a53
Amine <hamdi.amine.dev@gmail.com>
2020-09-20 13:12:46 +0100

    use webpack

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/react-terminal-view.js",
+  output: {
+    path: path.resolve("dist"),
+    filename: "react-terminal-view.js",
+    libraryTarget: "commonjs2"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules)/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["react"]
+            }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loader: "url-loader?name=images/[name].[ext]"
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      react: path.resolve(__dirname, "./node_modules/react"),
+      "react-dom": path.resolve(__dirname, "./node_modules/react-dom")
+    }
+  },
+  externals: {
+    // Don't bundle react or react-dom
+    react: {
+      commonjs: "react",
+      commonjs2: "react",
+      amd: "React",
+      root: "React"
+    },
+    "react-dom": {
+      commonjs: "react-dom",
+      commonjs2: "react-dom",
+      amd: "ReactDOM",
+      root: "ReactDOM"
+    }
+  }
+};
